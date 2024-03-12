f95c7a290cc1869f189a7401d67ddda62262c3c5
Troy <101336233+tmgkopczyk@users.noreply.github.com>
2024-03-12 10:05:04 -0400

    Figured out how to get title based on specific parameters

--- main.py
+++ main.py
@@ -39,13 +39,24 @@ def get_pptx_slides(presentation):
     slide_list = []
     for slide in presentation:
         slide_content = get_pptx_slide_content(slide)
-        slide_list.append(slide_content)
+        if any(slide_content.values()):
+            if slide_content.get("title") == "":
+                try:
+                    slide_content["title"] = slide_content["content"][0]
+                    slide_content["content"].pop(0)
+                except IndexError:
+                    pass
+            print(slide_content)
+            slide_list.append(slide_content)
+        else:
+            pass
     return slide_list
 
 def get_pptx_slide_content(slide):
     slide_dict = {
         "title":"",
-        "content":[]
+        "content":[],
+        "images":[]
     }
     try:
         title = slide.shapes.title.text
@@ -56,16 +67,68 @@ def get_pptx_slide_content(slide):
                 title = shape.text
                 break
     # using regex, find any unicode characters in the title that are outside the ASCII range and convert the title to a list of strings
-    regex = re.split(r'[^\x20-\x7E]', title)
+    regex = re.split(r"[\x0b+\xa0]",title)
     _slide_list = [x for x in regex if x != ""]
     if not _slide_list:
         pass
     else:
-        if len(_slide_list) == 1:
+        if len(_slide_list) == 2:
+            if (_slide_list[0][-1].islower() or _slide_list[0][-1].isupper() or (_slide_list[0][-1].isdigit() and _slide_list[1][0].isdigit() is False)) and (_slide_list[1][0].isupper() or _slide_list[1][0].isdigit()):
+                slide_dict["section"] = _slide_list[0]
+                slide_dict["title"] = _slide_list[-1]
+            elif _slide_list[0][-1] == " " or _slide_list[0][-1] == ":" or _slide_list[0][-1] == ",":
+                _slide_list = " ".join([x.strip() for x in _slide_list])
+                slide_dict["title"] = _slide_list
+            elif _slide_list[0][-1].islower() and _slide_list[1][0].islower():
+                _slide_list = " ".join([x.strip() for x in _slide_list if x.strip() != ""])
+                slide_dict["title"] = _slide_list
+            elif _slide_list[-1][0] == " " and _slide_list[0][-1] != " ":
+                _slide_list = " ".join([x.strip() for x in _slide_list if x.strip() != ""])
+                slide_dict["title"] = _slide_list
+            else:
+                _slide_list = " ".join([x.strip() for x in _slide_list])
+                slide_dict["title"] = _slide_list
+        elif len(_slide_list) == 1:
             slide_dict["title"] = _slide_list[0]
-        else:
-            if len(_slide_list) == 2:
-                if _slide_list[0][-1].islower() and _slide_list[1][0].isupper():
-                    print(_slide_list)
+        elif len(_slide_list) > 2:
+            if _slide_list[-1][0].islower():
+                slide_dict["title"] = " ".join([x.strip() for x in _slide_list if x.strip() != ""])
+            elif _slide_list[0][-1] == ":":
+                slide_dict["title"] = " ".join([x.strip() for x in _slide_list if x.strip() != ""])
+            # if the first character of each item in the list is an uppercase letter, then join the list into a string
+            elif _slide_list[0][0].isupper() and _slide_list[1][0].isupper():
+                slide_dict["title"] = " ".join([x.strip() for x in _slide_list if x.strip() != ""])
+            elif _slide_list[-2] == "–" or _slide_list[-2].islower():
+                slide_dict["title"] = " ".join([x.strip() for x in _slide_list if x.strip() != ""])
+            else:
+                slide_dict["title"] = " ".join([x.strip() for x in _slide_list if x.strip() != ""])
+    image_count = 0
+    for shape in slide.shapes:
+        if shape.has_text_frame:
+            if shape.text == title:
+                pass
+            else:
+                for paragraph in shape.text_frame.paragraphs:
+                    paragraph_text = ""
+                    if len(paragraph.text.strip()) <=2:
+                        pass
+                    elif paragraph.text.strip() == "":
+                        pass
+                    else:
+                        for run in paragraph.runs:
+                            if run.font.name is None:
+                                # get the font from the slide master
+                                for shape_l in slide.slide_layout.slide_master.slide_layouts.get_by_name(slide.slide_layout.name).shapes:
+                                    if shape_l.shape_type == shape.shape_type:
+                                        for paragraph_l in shape_l.text_frame.paragraphs:
+                                            for run_l in paragraph_l.runs:
+                                                print(run_l.font.bold)
+
+
+        elif shape.shape_type == 13:
+            image_count += 1
+            slide_dict["images"].append(f"{slide_dict['title'].lower().replace(' ','_')}_{image_count}")
+    return slide_dict
+
 if __name__ == "__main__":
     main()
